9b018c17067a4d91bda9f0d0ef276eed69eedf2d
Flax420 <rasmus.lillrank42@gmail.com>
2023-02-13 16:12:10 +0100

    Creating threads!

--- src/sthreads.c
+++ src/sthreads.c
@@ -27,7 +27,14 @@
                 Add data structures to manage the threads here.
 ********************************************************************************/
 
+typedef struct {
+  thread_t *first;
+  thread_t *last;
+}thread_list_t;
 
+int tid_counter = 1;
+thread_list_t *pool;
+ucontext_t main_context;
 
 /*******************************************************************************
                              Auxiliary functions
@@ -35,7 +42,46 @@
                       Add internal helper functions here.
 ********************************************************************************/
 
+void thread_list_push(thread_list_t *list , thread_t *thread) {
+  if(list->first == NULL) {
+    list->first = thread;
+    list->last = thread;
+    return;
+  }
+  list->last->next = thread;
+  list->last = thread;
+}
 
+thread_t *thread_list_pop(thread_list_t *list) {
+  if(list->first == NULL) { // List empty
+    return NULL;
+  } else if(list->first->next == NULL) { // only element
+    thread_t *first = list->first;
+    list->first = NULL;
+    list->last = NULL;
+    return first;
+  }
+  thread_t *first = list->first;
+  list->first = first->next;
+  return first;
+}
+
+void create_context(ucontext_t *ctx) {
+  ctx->uc_stack.ss_sp = calloc(STACK_SIZE, sizeof(char));
+  ctx->uc_stack.ss_size = STACK_SIZE;
+  ctx->uc_link = &main_context;
+}
+
+thread_t *create_thread(void (*start)()){
+  thread_t *thread = calloc(1, sizeof(thread_t));
+  thread->tid = tid_counter;
+  thread->state = waiting;
+  getcontext(&thread->ctx);
+  create_context(&thread->ctx);
+  makecontext(&thread->ctx, start, -1); 
+  tid_counter++;
+  return thread;
+}
 
 
 /*******************************************************************************
@@ -43,13 +89,23 @@
 ********************************************************************************/
 
 
-int  init(){
+int init(){
+  pool = calloc(1, sizeof(pool));
+  pool->first = NULL;
+  pool->last = NULL;
+  getcontext(&main_context);
   return 1;
 }
 
 
 tid_t spawn(void (*start)()){
-  return -1;
+  thread_t *thread = create_thread(start);
+  thread_list_push(pool, thread);
+  pool->first->state = running;
+  if(swapcontext(&main_context, &pool->first->ctx)) { //if fail
+    return -1;
+  }
+  return thread->tid;
 }
 
 void yield(){
@@ -58,6 +114,6 @@ void yield(){
 void  done(){
 }
 
-tid_t join() {
+tid_t join(tid_t thread_t) {
   return -1;
 }
